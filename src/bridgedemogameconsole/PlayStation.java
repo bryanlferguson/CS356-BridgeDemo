0d2cb8c972df4ef9aae9b5de59291c21351d6dea
fergo <fergo@10.110.66.127>
2018-07-19 09:36:48 -0700

    fixed typo

--- src/bridgedemogameconsole/PlayStation.java
+++ src/bridgedemogameconsole/PlayStation.java
@@ -4,7 +4,7 @@ public class PlayStation extends GameConsole {
 
 	@Override
 	public void playGame(String gameTitle, int numPlayers) {
-		System.out.println("PlayStation is now playing " + gameTitle + "with " + numPlayers + "number of players.");
+		System.out.println("PlayStation is now playing " + gameTitle + " with " + numPlayers + " players.");
 	}
 
 	@Override
